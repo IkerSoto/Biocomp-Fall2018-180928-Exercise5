55aa059b022d21ebf1d877a69d92a184671b780d
Iker Soto <ikerfrancisco@gmail.com>
2018-10-04 23:45:34 -0400

    Activity 1 and 2 (gender-yearsExp.sh)

--- gender-yearsExp.sh
+++ gender-yearsExp.sh
@@ -1,7 +1,7 @@
 #Authors: Ellen Foley & Iker Soto
 #Activity 1
 #Creates a file that contains the unique gender-years experience combinations present in the wages.csv
-#Usage: bash gender-yearsExp wages.csv
+#Usage: bash gender-yearsExp.sh wages.csv
 
 cat $@ |cut -d , -f1,2 |sed 's/,/ /g' |sed '/gender/d'|sort -k1,1 -k2,2n |uniq >gender-yearsExp.csv
 
