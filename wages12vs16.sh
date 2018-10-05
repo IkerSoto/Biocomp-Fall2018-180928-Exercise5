abc66fccb3d7b65592c67e0266bb79a0a5bf075a
Ellen Foley <efoley5@nd.edu>
2018-10-04 22:51:21 -0400

    UPDATED. Value for min college wage - min non college wage

--- wages12vs16.sh
+++ wages12vs16.sh
@@ -1,8 +1,8 @@
 # college degree minimum wage - no college minimum wage. 
 
 echo "The difference in minimum wage between college and non college educated persons in this data set is" 
-var1="$(cat wages.csv |cut -d , -f3,4 |grep -E ^12 |cut -d , -f2|sort -n |head -n 1)
-var2="$(cat wages.csv |cut -d , -f3,4 |grep -E ^16 |cut -d, -f2 |sort -n |head -n 1)
-echo "$var2 - $var1" | bc
+val1=$(cat wages.csv |cut -d , -f3,4 |grep -E ^12 |cut -d , -f2|sort -n |head -n 1)
+val2=$(cat wages.csv |cut -d , -f3,4 |grep -E ^16 |cut -d, -f2 |sort -n |head -n 1)
+echo "$val2 - $val1" |bc
 
 
