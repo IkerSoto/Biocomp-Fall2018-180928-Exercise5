088c7f6658f587f7e6dbb9f8819f2358d9809b8c
Ellen Foley <efoley5@nd.edu>
2018-10-04 22:37:48 -0400

    Ellen- Difference in min wage of college and non college wages

--- wages12vs16.sh
+++ wages12vs16.sh
@@ -1,14 +1,8 @@
-# I managed to write a script that sends to the output the list of wages people that went to college and people that didn't earn, however I'm struggling with the next part, I'm assuming we need to get the averages in order to substract one from the other but I'm not sure how to do that, any ideas? or maybe I'm understanding the exercise wrong?   
+# college degree minimum wage - no college minimum wage. 
+
+echo "The difference in minimum wage between college and non college educated persons in this data set is" 
+var1="$(cat wages.csv |cut -d , -f3,4 |grep -E ^12 |cut -d , -f2|sort -n |head -n 1)
+var2="$(cat wages.csv |cut -d , -f3,4 |grep -E ^16 |cut -d, -f2 |sort -n |head -n 1)
+echo "$var2 - $var1" | bc
 
 
-
-
-for file in $1
-do
-echo "Wages of people that didn't go to college"
-cat $file |cut -d , -f3,4 |grep -E ^12 |cut -d , -f2
-echo "Wages of people that went to college"  
-cat $file |cut -d , -f3,4 |grep -E ^16 |cut -d, -f2
-done
-
-# ...?
